ff4503238c79fbc762b9c421406ecc187a5e0a30
degarti <outragedline@proton.me>
2025-10-05 19:28:44 -0300

    docs: Add simple example

---
+++ karga_core/examples/http.rs
@@ -0,0 +1,38 @@
+use std::time::{Duration, Instant};
+
+use karga::{
+    executor::ConstantExecutor,
+    metrics::{BasicAggregate, BasicMetric, JsonReport, Report},
+    scenario::Scenario,
+};
+use reqwest::Client;
+
+#[tokio::main]
+async fn main() {
+    let results = Scenario::<BasicAggregate, _, _, _>::builder()
+        .name("Basic scenario")
+        .action(async || {
+            let client = Client::new();
+            let start = Instant::now();
+
+            // Yeah lets hardcode it
+            let res = client.get("http://localhost:3000").send().await;
+            let success = match res {
+                Ok(r) => r.status() == 200,
+                Err(_) => false,
+            };
+            let elapsed = start.elapsed();
+            BasicMetric {
+                latency: elapsed,
+                success,
+                bytes: 0,
+            }
+        })
+        .executor(ConstantExecutor::new(Duration::from_secs(10), 12))
+        .build()
+        .run()
+        .await
+        .unwrap();
+
+    JsonReport::from(results).report().await.unwrap();
+}
