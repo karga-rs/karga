05e2a01f00828430f20a918393cddc813b085c90
degarti <outragedline@proton.me>
2025-10-06 10:32:33 -0300

    docs: Improve example performance from 2k reqs to 600k reqs

--- karga/examples/http.rs
+++ karga/examples/http.rs
@@ -9,23 +9,26 @@ use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
+    let client = Client::new();
     let results = Scenario::<BasicAggregate, _, _, _>::builder()
         .name("Basic scenario")
-        .action(async || {
-            let client = Client::new();
-            let start = Instant::now();
+        .action(move || {
+            let client = client.clone();
+            async move {
+                let start = Instant::now();
 
-            // Yeah lets hardcode it
-            let res = client.get("http://localhost:3000").send().await;
-            let success = match res {
-                Ok(r) => r.status() == 200,
-                Err(_) => false,
-            };
-            let elapsed = start.elapsed();
-            BasicMetric {
-                latency: elapsed,
-                success,
-                bytes: 0,
+                // Yeah lets hardcode it
+                let res = client.get("http://localhost:3000").send().await;
+                let success = match res {
+                    Ok(r) => r.status() == 200,
+                    Err(_) => false,
+                };
+                let elapsed = start.elapsed();
+                BasicMetric {
+                    latency: elapsed,
+                    success,
+                    bytes: 0,
+                }
             }
         })
         .executor(ConstantExecutor::new(Duration::from_secs(10), 12))
